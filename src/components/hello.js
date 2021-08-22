5389ee0349abdb0643f5ee72c0060df25a3c19d4
Grant Holley <gvholley@gmail.com>
2021-08-22 15:15:51 +0900

    Updates

--- src/components/hello.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class Hello extends React.Component {
-  // eslint-disable-next-line react/require-render-return
-  render() {
-    return <div>Hello {this.props.name}</div>
-  }
-}
-
-export default Hello;
